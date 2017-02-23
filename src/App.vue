d4556618cb014a0c45bbe635497d40885bd58375
Chan-Chun <362732414@qq.com>
2017-02-23 10:34:31 +0800

    Regular Commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <leftNav class="leftNav"></leftNav>
-    <router-view class="routerView"></router-view>
+    <div class="container">
+      <leftNav class="leftNav"></leftNav>
+      <router-view class="routerView"></router-view>
+    </div>
   </div>
 </template>
 
@@ -15,7 +17,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,24 +26,39 @@ export default {
   /*color: #2c3e50;*/
   /*margin-top: 60px;*/
   height: inherit;
+
+  .container{
+    height: inherit;
+    position: relative;
+    .leftNav {
+      height:50%;
+      width:10%;
+      border: 1px solid black;
+      overflow: auto;
+      margin: auto;
+      position: absolute;
+      top: 0; left: 0; bottom: 0; right: 80%;
+    }
+
+    .routerView {
+      border: 1px solid black;
+      width: 60%;
+      height: 70%;
+      overflow: auto;
+      margin: auto;
+      position: absolute;
+      top: 0; left: 10%; bottom: 0; right: 0;
+    }
+  }
 }
 
 html, body {
-  height:100%;  
+  height:100%;
   width:100%;
   background-color: ghostwhite;
   overflow: hidden;
 }
 
-.leftNav{
-  position: absolute;
-  border: 1px solid black;
-}
 
-.routerView{
-  height: 70%;
-  width: 70%;
-  border:1px solid black;
-}
 
 </style>
